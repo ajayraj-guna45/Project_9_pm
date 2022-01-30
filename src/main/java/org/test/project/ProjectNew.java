e0e3e3e9c678d34cdcaea3cb59c1b2fd856f16e6
ajayraj.guna45 <ajayraj.guna45@gmail.com>
2022-01-30 18:10:33 +0530

    mano's Update Code

--- src/main/java/org/test/project/ProjectNew.java
+++ src/main/java/org/test/project/ProjectNew.java
@@ -2,6 +2,8 @@ package org.test.project;
 
 public class ProjectNew {
 public static void main(String[] args) {
+	System.out.println("old code");
 	System.out.println("New code");
+	System.out.println("day code");
 }
 }

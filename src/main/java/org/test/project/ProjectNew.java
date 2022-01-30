a44afc4e89c47e9832a1fb50c4196c2deaa66b8d
ajayraj.guna45 <ajayraj.guna45@gmail.com>
2022-01-30 18:16:52 +0530

    raj's Update Code

--- src/main/java/org/test/project/ProjectNew.java
+++ src/main/java/org/test/project/ProjectNew.java
@@ -2,6 +2,11 @@ package org.test.project;
 
 public class ProjectNew {
 public static void main(String[] args) {
-	System.out.println("New code");
+	System.out.println("1 code");
+	System.out.println("2 code");
+	System.out.println("3 code");
+	System.out.println("4 code");
+	System.out.println("5 code");
+	System.out.println("6 code");
 }
 }
